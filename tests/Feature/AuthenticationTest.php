e4d1474e6198e716e3b7f08b24e796fa383c4f77
Devender Gupta <degeta10@gmail.com>
2022-01-08 23:31:01 +0530

    Add test cases for logout and refresh token

--- tests/Feature/AuthenticationTest.php
+++ tests/Feature/AuthenticationTest.php
@@ -3,6 +3,7 @@
 namespace Tests\Feature;
 
 use Illuminate\Foundation\Testing\RefreshDatabase;
+use Laravel\Passport\Passport;
 use Tests\TestCase;
 
 class AuthenticationTest extends TestCase
@@ -18,8 +19,7 @@ class AuthenticationTest extends TestCase
             [
                 'email'     => $user->email,
                 'password'  => 'password'
-            ],
-            ['Accept' => 'application/json']
+            ]
         )->assertStatus(200)->assertJsonStructure([
             "data" => [
                 "message",
@@ -48,8 +48,7 @@ class AuthenticationTest extends TestCase
                 'email'                 => 'test@test.com',
                 'password'              => 'password',
                 'password_confirmation' => 'password',
-            ],
-            ['Accept' => 'application/json']
+            ]
         )->assertStatus(200);
         $this->artisan('migrate:reset');
     }
@@ -65,9 +64,8 @@ class AuthenticationTest extends TestCase
             [
                 'email'     => $user->email,
                 'password'  => 'password1'
-            ],
-            ['Accept' => 'application/json']
-        )->assertStatus(401);
+            ]
+        )->assertUnauthorized();
         $this->artisan('migrate:reset');
     }
 
@@ -83,8 +81,60 @@ class AuthenticationTest extends TestCase
                 'email'     => $user->email . "test",
                 'password'  => 'password1'
             ],
-            ['Accept' => 'application/json']
-        )->assertStatus(422);
+        )->assertUnprocessable();
+        $this->artisan('migrate:reset');
+    }
+
+    public function test_loggedin_user_can_logout_using_api()
+    {
+        $this->withoutExceptionHandling();
+        $this->artisan('migrate:fresh');
+        $this->artisan('passport:install');
+
+        Passport::actingAs(
+            \App\Models\User::factory()->create(),
+            ['*']
+        );
+
+        $this->postJson('/api/auth/logout')->assertStatus(200);
+        $this->artisan('migrate:reset');
+    }
+
+    public function test_guest_user_cannot_logout_using_api()
+    {
+        $this->withoutExceptionHandling();
+        $this->expectException('Illuminate\Auth\AuthenticationException');
+        $this->postJson('/api/auth/logout');
+    }
+
+    public function test_loggedin_user_can_refresh_token_using_api()
+    {
+        $this->withoutExceptionHandling();
+        $this->artisan('migrate:fresh');
+        $this->artisan('passport:install');
+        $user = \App\Models\User::factory()->create();
+        $response = $this->postJson(
+            '/api/auth/login',
+            [
+                'email'     => $user->email,
+                'password'  => 'password'
+            ]
+        );
+        $refreshToken = $response['data']['refresh_token'];
+        $this->postJson(
+            '/api/auth/token/refresh',
+            ['token' => $refreshToken]
+        )
+            ->assertStatus(200)
+            ->assertJsonStructure([
+                "data" => [
+                    "message",
+                    "token_type",
+                    "access_token",
+                    "refresh_token",
+                    "expires_at",
+                ]
+            ]);
         $this->artisan('migrate:reset');
     }
 }
